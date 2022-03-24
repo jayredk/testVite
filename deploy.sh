2179a5adbb82a90b75ef77f294e301e2ffedb51e
Jay <kred110396@gmail.com>
2022-03-24 14:36:46 +0800

    Initial commit

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git checkout -b main
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:jayredk/testVite.git main:gh-pages
+
+cd -
